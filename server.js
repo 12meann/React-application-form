e210081354badd731f03b0e09c8f3fa584e49f43
Me-ann Urgel <meann1_2@yahoo.com>
2019-06-28 21:13:16 +0800

    try again if will work

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const Form = require("./model/Form");
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
-  console.log("Connected with mongoDB");
+  console.log("Mongodb connected");
 });
 
 app.post("/", (req, res) => {
